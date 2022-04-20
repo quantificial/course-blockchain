015cc63744208ca8c2a3229327aba6ac9934d809
Johnson Fu <johnson.fu@gmail.com>
2022-04-20 23:26:27 +0800

    update react project

--- Etherium/lottery-updated/deploy.js
+++ Etherium/lottery-updated/deploy.js
@@ -1,13 +1,14 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
-  '',
+  'YOUR_MNEMONIC',
   // remember to change this to your own phrase!
-  ''
+  'YOUR_INFURA_URL'
   // remember to change this to your own endpoint!
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -15,10 +16,11 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
     .send({ gas: '1000000', from: accounts[0] });
 
+  console.log(JSON.stringify(abi));
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
